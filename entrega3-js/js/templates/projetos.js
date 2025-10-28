832eb1aeb5d9f90922d840dd0b80d54873388495
Larissa <larissavi25ss@icloud.com>
2025-10-28 16:58:31 -0300

    Create projetos.js

---
+++ entrega3-js/js/templates/projetos.js
@@ -0,0 +1,27 @@
+// js/templates/projetos.js
+export const template = `
+<section class="projetos">
+  <h2>Projetos da ONG</h2>
+  <p>Conheça as ações que fazem a diferença em nossa comunidade.</p>
+
+  <div class="cards-container">
+    <div class="card">
+      <img src="images/projeto1.jpg" alt="Projeto 1">
+      <h3>Educação para Todos</h3>
+      <p>Aulas de reforço escolar para crianças em situação de vulnerabilidade.</p>
+    </div>
+
+    <div class="card">
+      <img src="images/projeto2.jpg" alt="Projeto 2">
+      <h3>Alimente Esperança</h3>
+      <p>Distribuição de cestas básicas a famílias carentes.</p>
+    </div>
+
+    <div class="card">
+      <img src="images/projeto3.jpg" alt="Projeto 3">
+      <h3>Saúde Solidária</h3>
+      <p>Campanhas de vacinação e atendimento médico gratuito.</p>
+    </div>
+  </div>
+</section>
+`;
