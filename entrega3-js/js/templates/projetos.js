fc0e746193a017805000ff2d6a16612f43d65c65
Larissa <larissavi25ss@icloud.com>
2025-10-28 20:39:42 -0300

    Update projetos.js

--- entrega3-js/js/templates/projetos.js
+++ entrega3-js/js/templates/projetos.js
@@ -1,27 +1,22 @@
-// Template atualizado para os projetos
 export const template = `
 <section class="projetos">
   <h2>Projetos da ONG</h2>
   <p>Conheça as ações que fazem a diferença em nossa comunidade.</p>
 
-  <div class="cards-projetos">
+  <div class="cards-container">
+    <div class="card">
+      <img src="images/projeto-1.jpg" alt="Projeto 1">
+      <img src="images/projetos/projeto-1.jpg" alt="Projeto 1">
+      <h3>Educação para Todos</h3>
+      <p>Aulas de reforço escolar para crianças em situação de vulnerabilidade.</p>
+    </div>
 
-    <article class="card">
-      <img src="images/projetos/projeto-1.jpg" alt="Educação para Todos">
-      <div class="card-content">
-        <h3>Educação para Todos</h3>
-        <p>Aulas de reforço escolar para crianças em situação de vulnerabilidade.</p>
-      </div>
-    </article>
-
-    <article class="card">
-      <img src="images/projetos/projeto-2.jpg" alt="Projeto Saúde">
-      <div class="card-content">
-        <h3>Projeto Saúde</h3>
-        <p>Campanhas de prevenção e apoio à saúde em comunidades carentes.</p>
-      </div>
-    </article>
-
+    <div class="card">
+      <img src="images/projeto-2.jpg" alt="Projeto 2">
+      <img src="images/projetos/projeto-2.jpg" alt="Projeto 2">
+      <h3>Projeto Saúde</h3>
+      <p>Campanhas de prevenção e apoio à saúde em comunidades carentes.</p>
+    </div>
   </div>
 </section>
 `;
