90d03005a3197e54d3a73b3f3b4d44d181130272
Larissa <larissavi25ss@icloud.com>
2025-10-28 18:13:50 -0300

    Update home.js

--- entrega3-js/js/templates/home.js
+++ entrega3-js/js/templates/home.js
@@ -1,16 +1,15 @@
-// js/templates/home.js
 export const template = `
 <section class="home">
   <h1>Bem-vindo à ONG Exemplo</h1>
   <p>Transformando vidas através do voluntariado e das doações.</p>
 
   <div class="banner">
-    <img src="images/banner.jpg" alt="Banner da ONG" class="img-banner">
+    <img src="images/logo.png" alt="Logo da ONG" class="img-banner">
   </div>
 
   <div class="cta">
-    <a href="/projetos" data-link class="btn">Conheça nossos projetos</a>
-    <a href="/cadastro" data-link class="btn btn-secundario">Seja um voluntário</a>
+    <a href="/entrega3-js/projetos" data-link class="btn">Conheça nossos projetos</a>
+    <a href="/entrega3-js/cadastro" data-link class="btn btn-secundario">Seja um voluntário</a>
   </div>
 </section>
 `;
