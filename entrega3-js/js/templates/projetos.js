3a236cc07cffa46672950942a766abf63703e330
Larissa <larissavi25ss@icloud.com>
2025-10-28 20:09:06 -0300

    Update projetos.js

--- entrega3-js/js/templates/projetos.js
+++ entrega3-js/js/templates/projetos.js
@@ -5,13 +5,13 @@ export const template = `
 
   <div class="cards-container">
     <div class="card">
-      <img src="images/projeto-1.jpg" alt="Projeto 1">
+      <img src="images/projetos/projeto-1.jpg" alt="Projeto 1">
       <h3>Educação para Todos</h3>
       <p>Aulas de reforço escolar para crianças em situação de vulnerabilidade.</p>
     </div>
 
     <div class="card">
-      <img src="images/projeto-2.jpg" alt="Projeto 2">
+      <img src="images/projetos/projeto-2.jpg" alt="Projeto 2">
       <h3>Alimente Esperança</h3>
       <p>Distribuição de cestas básicas a famílias carentes.</p>
     </div>
